55604fe42e25e04ab78f630c72200898382b659b
Elijah Ackah <70490677+EliAckah@users.noreply.github.com>
2025-02-18 09:48:08 +0000

    declare global variables

---
+++ script.js
@@ -0,0 +1,4 @@
+// declare global variables
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
