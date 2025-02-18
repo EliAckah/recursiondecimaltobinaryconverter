651dbf045e7c0fa5fcf91d56a222fe18af7e5b6a
Elijah Ackah <70490677+EliAckah@users.noreply.github.com>
2025-02-18 10:26:31 +0000

    function to check the value in the number input

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+
+// function to check the value in the number input
+const checkUserInput = () => {
+    if (!numberInput.value) {
+
+    }
+    console.log(numberInput.value);
+}
+
+convertBtn.addEventListener("click", checkUserInput);
+numberInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        checkUserInput();
+    }
+});
